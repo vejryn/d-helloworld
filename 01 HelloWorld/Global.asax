d28a0c81cdaab203089a99a289c521896e7fb30b
Vejrynsh <Vejrynsh@DESKTOP-O4DVL5C>
2017-01-08 22:30:28 +0700

    Add project files.

---
+++ 01 HelloWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_01_HelloWorld.MvcApplication" Language="C#" %>
---
+++ 01 HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_01_HelloWorld.Startup))]
+namespace _01_HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
